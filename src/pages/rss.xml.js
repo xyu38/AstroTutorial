daa5014b272fa7275b63dbb4a50992f47f872fff
Xuying Yu <113731556+xyu38@users.noreply.github.com>
2022-12-29 17:36:56 -0800

    created a .xml feed doc

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,9 @@
+import rss from '@astrojs/rss';
+
+export const get = () => rss({
+  title: 'Astro Learner | Blog',
+  description: 'My journey learning Astro',
+  site: 'https://yuki137.netlify.app',
+  items: import.meta.glob('./**/*.md'),
+  customData: `<language>en-us</language>`,
+});
